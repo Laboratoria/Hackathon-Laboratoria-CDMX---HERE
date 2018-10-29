2f8093d20fdfe5b15575899b5589b4a105a824f0
florenciasilva <hensgrej@gmail.com>
2018-10-29 16:28:52 -0600

    Routeo de Form

---
+++ hackathon/src/Components/Admin/Form.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Row, Input} from 'react-materialize';
+
+const Form = () => (
+    <div>
+        <p>holi 2</p>
+    </div>
+)
+
+export default Form
--- hackathon/src/routes.js
+++ hackathon/src/routes.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import {BrowserRouter as Router,  Route, Switch} from 'react-router-dom';
 import Login from './Components/Global/Login';
-import LandingPage from './Components/Global/LandingPage'
-
+import LandingPage from './Components/Global/LandingPage';
+import Form from './Components/Admin/Form';
 
         const login = () => (
             <div>
                 <Login />
             </div>
-        )
+        );
 
         const landing = () => (
             <div>
                 <LandingPage />
             </div>
-        )
+        );
+
+        const registerInstitution = () => (
+            <div>
+                <Form />
+            </div>
+        );
 
 const Routes = () => (
     <Router basename={process.env.PUBLIC_URL}>      
             <Switch>
                 <Route exact strict path="/" component= {landing} />                
                 <Route exact strict path="/login" component= {login} />
+                <Route exact strict path="/register-institution" component= {registerInstitution} />
             </Switch>
     </Router>   
 );
